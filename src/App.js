543bb73ed20f8fbf2587806339c9eb8a22ae25d0
dantasyisland <dantothelastdrop@yahoo.com>
2022-03-29 10:54:06 -0500

    planned build - updated document title in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
+import apiKey from './config';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      firstState: "Delete this",
+      firstState: 'Delete this',
     };
   }
+
+  componentDidMount() {
+    document.title = 'React Flickr Page';
+  }
   render() {
-    return <div>APP</div>;
+    return (
+      <div className='container'>
+        <h1>Welcome</h1>
+      </div>
+    );
   }
 }
